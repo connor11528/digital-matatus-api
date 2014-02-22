515c5917e7665fd1d5a9c9cec7cf4ecf8c5b47e7
Connor James Leech <connorleech@gmail.com>
2014-02-22 17:01:11 +0300

    still broken

--- readShapes.js
+++ readShapes.js
@@ -10,32 +10,44 @@ fs.readFile('data/shapes.txt', 'utf-8', function(err, data){
 		var shape = lines[j].split(',');
 		if (shape_ids.indexOf(shape[0]) > -1){
 			
-			
 		} else {
 			shape_ids.push(shape[0]);
 		}
 	}
 	
-	// add all corresponding points
-	for(var j = 0; j < lines.length; j++){
-		
-		var shape = lines[j].split(',');
-		if(shape_ids.indexOf(shape[0])){
-			
-		}
-			if(shape[0] === shape_ids[i]){
-				listOfPoints.push({
-					lat:shape[1],
-					lon:shape[2]
-				});
-				console.log(listOfPoints);
-			}
-	}
+	console.log(shape_ids);
 	
-	waypoints = JSON.stringify(waypoints, null, 4);	// make array pretty
+	// add all corresponding points
+	for(var i = 0; i < shape_ids.length; i++){
+		var shape_id = shape_ids[i];
+		
+		
+		for(var j = 0; j < lines.length; j++){
+			var line = lines[j].split(',');
+			var waypoints = [];
+			if(shape_id === line[0]){
+				waypoints.push(line[0]);
+				waypoints = JSON.stringify(waypoints, null, 4);	// make array pretty
 			
-	fs.writeFile('output2.txt', waypoints, function(err){
-		if (err) throw err;
-		console.log('Wrote a route on output2.txt');
-	});
+				fs.writeFile('output2.txt', waypoints, function(err){
+					if (err) throw err;
+					console.log('Wrote a route on output2.txt');
+				});
+			}
+		}
+	}
 });
+
+function Shape(shape_id){
+	this.shape_id = shape_id;
+	this.waypoints = [];
+}
+
+
+
+
+
+
+
+
+
