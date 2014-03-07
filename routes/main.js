d5fed0cfd54f6efd2935b98020a317b61e3e20f6
Connor James Leech <connorleech@gmail.com>
2014-03-07 10:06:28 +0300

    add angularjs

--- routes/main.js
+++ routes/main.js
@@ -1,3 +1,3 @@
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+	res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose');
-
+var port = process.env.PORT || 3000;
 var main = require('./routes/main');
 var user = require('./routes/user');
 var route = require('./routes/route');
@@ -10,18 +10,13 @@ var route = require('./routes/route');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.favicon());
+
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
@@ -29,18 +24,18 @@ if ('development' == app.get('env')) {
   mongoose.connect('mongodb://localhost/opendatake');
 }
 
-// ROUTES
-app.get('/', main.index);
+// API Routes
 app.get('/users', user.findAll);
-
-// Route routes
 app.get('/routes', route.findAll);
 app.get('/routes/:id', route.findById);
 app.post('/routes', route.addRoute);
 app.put('/route/:id', route.updateRoute);
 app.delete('/route/:id', route.deleteRoute);
 
+// everything else goes to index.html
+app.get('/*', main.index);
+
 // start server
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(port, function(){
+  console.log('Express server listening on port ' + port);
 });
