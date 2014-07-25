21d02fa3a5bf3d6559bf4a09defdca7e3555a024
Connor James Leech <connorleech@gmail.com>
2014-07-24 17:09:40 -0700

    render map wrking

--- public/scripts/directives/leaflet-map.js
+++ public/scripts/directives/leaflet-map.js
@@ -1,12 +1,20 @@
 'use strict';
 
-app.directive('leaflet-map', function(){
+app.directive('leafletMap', function(){
+	// map layer from stamen: https://www.mapbox.com/mapbox.js/example/v1.0.0/external-layers/
+	var stamenUrl = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png';
+
 	return {
 		restrict: 'A',
 		link: function(scope, elem, attrs){
+			// create map
 			var nairobi = [-1.282836, 36.829201]
-			var map = L.map(elem).setView(nairobi, 12);
-			L.tileLayer('http://{s}.tile.cloudmade.com/2515f952e6c3418488627c3e24cd773b/997/256/{z}/{x}/{y}.png', {
+			var map = L.map(elem[0].id).setView(nairobi, 12);
+
+			console.log(elem)
+			
+			// add tile layer
+			L.tileLayer(stamenUrl, {
 			    maxZoom: 18
 			}).addTo(map);
 		}
